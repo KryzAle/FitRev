9543c834a2d6b4f64b728a8523f34fc3b242d0f4
Kryz Ale <kryzale@gmail.com>
2018-11-14 07:27:38 -0500

    Agregada la layout y view iniciales

---
+++ FitRev/Controllers/PrincipalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FitRev.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        // GET: Principal
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+    }
+}
