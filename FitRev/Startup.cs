2719e45a005178c3e75ceeebeeeb99c8c0b28ccb
Kryz Ale <kryzale@gmail.com>
2018-11-14 08:18:28 -0500

    Login y register implementados
    La layout de la plantilla ha sido suprimida y ha quedado la plantilla por defecto.

---
+++ FitRev/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitRev.Startup))]
+namespace FitRev
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
